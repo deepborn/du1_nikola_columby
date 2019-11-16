8c3ef837cec7a14a9a70efd71942c8c08d01ab91
deepborn <columbyn@gmail.com>
2019-11-16 12:12:51 +0100

    -opravil vzorce (byly spatne) -vymyslel vybirani zobrazeni -meritko hotovy -jdu na baleni vzorcu do funkci

--- du1_hratky.py
+++ du1_hratky.py
@@ -15,6 +15,7 @@
 # tan(v radianech) vypocita tangens
 # log(x, ...) kdyz necham ... prazdny, bude zakladni
 
+
 import math
 # help(math)
 
@@ -22,7 +23,7 @@ R = 6371.11
 v = 10
 u = 180 # dosadim "u" a zjistim, kam mam udelat carku na ose y a tam na to budu kolmo dělat rovnobezky
 d = 90 - u
-
+"""
 print(R*math.radians(v)) # = x, platici pro vsechny zobrazeni
 # vypocita polohu na rovniku (osa x), kde kolmo nacrtne polednik
 
@@ -30,7 +31,7 @@ print(R*math.sin(math.radians(u))) # = y, pro lambertovo zobrazeni
 print(R*(math.radians(u))) # = y, pro marinovo zobrazeni
 print(2*R*math.tan(math.radians(u/2))) # = y, pro braunovo zobrazeni
 print(R*math.log(1/math.tan(math.radians(d/2))))  # = y, pro mercatorovo zobrazeni
-
+"""
 # jak resit meritko
 # x = ((input("Zadej měřítko, ve kterém chceš výsledek, \n\
 # ve formě 1 : x, celočíselně, zadej pouze x, bez mezer: "))
@@ -38,7 +39,8 @@ print(R*math.log(1/math.tan(math.radians(d/2))))  # = y, pro mercatorovo zobraze
 # uzivatel mi zada meritko, ja ho vydelim 100000 a vznikne mi x/100000
 # tim kdyz si vydelim vysledek vzorce, dostanu ho v [cm]
 
-
+# vypisovani rovnobezek ci poledniku v seznamu
+"""
 rovnobezky = [] #seznam rovnobezek, jen stupne
 deg_r = -100
 for _ in range(19):
@@ -46,7 +48,6 @@ for _ in range(19):
     rovnobezky.append(deg_r)
 print(rovnobezky)
 
-#
 
 poledniky = [] #seznam poledniku, jenom stupne
 deg_p = -190
@@ -54,3 +55,31 @@ for i in range(37):
     deg_p = (deg_p + 10)
     poledniky.append(deg_p)
 print(poledniky)
+"""
+
+# vybirani zobrazeni
+"""
+zobrazeni = str(input("Vyber si zobrazení! Pro Lambertovo napiš L, pro Marinovo napiš A,\
+ pro Braunovo napiš B, pro Mercatorovo napiš M :" )).upper()
+
+while True:
+    if zobrazeni == "L":
+        print("LAMBERT")
+        break
+    if zobrazeni == "A":
+        print("MARIN")
+        break
+    if zobrazeni == "B":
+        print("BRAUN")
+        break
+    if zobrazeni == "M":
+        print("MERCATOR")
+        break
+    else:
+        print("SPATNE JSI TO ZADAL")
+        break
+"""
+
+
+
+
--- du_1.py
+++ du_1.py
@@ -4,33 +4,65 @@
 # ulozit si to do seznamu s urcitym poradim, bod 0,0 nejak dat doprostred?
 # meritko si vybere, to se jen ulozi do promenny a pak se tim proste vydeli ten vysledek
 
-
 # rovnobezky i poledniky po 10deg --> for cyklus
 # ale chci to mit rovnobezky -90 az 90 a poledniky -180 az 180 pres bod [0,0] a to musi bejt stred papiru
 
-import math
-
-R = 6371.11
-# print(R*math.radians(10)) # = x
+# print(R*math.radians(u)) # = x
 # print(R*math.sin(math.radians(u))) # = y, pro lambertovo zobrazeni
+# x je poloha na rovniku a urcuje, kde mam kolmo na to nakreslit POLEDNIK
+# y je poloha na nultym poledniku a urcuje, kde mam kolmo na to nakreslit ROVNOBEZKU (tech je min a ty se meni)jf
+
+import math
+R = 6371.11
+
+# vyber zobrazeni uzivatelem
+
+zobrazeni = str(input("Vyber si zobrazení! Pro Lambertovo napiš L, pro Marinovo napiš A,\
+ pro Braunovo napiš B, pro Mercatorovo napiš M :" )).upper()
+
+while True:
+    if zobrazeni == "L":
+        print("LAMBERT")
+        break
+    if zobrazeni == "A":
+        print("MARIN")
+        break
+    if zobrazeni == "B":
+        print("BRAUN")
+        break
+    if zobrazeni == "M":
+        print("MERCATOR")
+        break
+    else:
+        print("SPATNE JSI TO ZADAL")
+        break
+
+# uzivatel zada meritko a hned se z nej vypocita cislo, kterym se prevadi do [cm]
+
+x = (float((input("Zadej měřítko, ve kterém chceš výsledek, ve formě 1 : x, celočíselně, zadej pouze x, bez mezer: ")))/100000)
+
+# pocitadlo rovnobezek pro LAMBERT
 
 rovnobezky = [] #seznam rovnobezek, uz se vzorcem
 deg_r = -100
 
-x = (float((input("Zadej měřítko, ve kterém chceš výsledek, ve formě 1 : x, celočíselně, zadej pouze x, bez mezer: ")))/100000)
-print(x)
-
-for _ in range(19):
+for _ in range(19):     # tohle je vzorec x, ten stejnej
     deg_r = (deg_r + 10)
-    rovnobezka = round(((R*math.radians(deg_r))/x), ndigits=2)
+    rovnobezka = round(((R*(math.sin(math.radians(deg_r))))/x), ndigits=1)
     rovnobezky.append(rovnobezka)
 print(rovnobezky)
+print(deg_r)
+
+# pocitadlo poledniku - vsechny zobrazeni STEJNY
 
 poledniky = []
 deg_p = -190
 
-for _ in range(37):
+for i in range(37):     # tohle je vzorec y, ten, co se meni
     deg_p = (deg_p + 10)
-    polednik = round(R*math.sin(math.radians(deg_p)), ndigits=2)
+    polednik = round(((R*(math.radians(deg_p))/x)), ndigits=1)
     poledniky.append(polednik)
 print(poledniky)
+print(deg_p)
+
+
